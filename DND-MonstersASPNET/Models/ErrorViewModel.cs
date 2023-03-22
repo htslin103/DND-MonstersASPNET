3251e6e44a4de55a1534dfcc4586eb0eb6209c8f
Helen L <htslin103@gmail.com>
2023-03-21 21:43:03 -0600

    Add project files.

---
+++ DND-MonstersASPNET/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DND_MonstersASPNET.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

7de22cbf83409fb0c3f73f88366d7a5fa276c0c2
Helen L <htslin103@gmail.com>
2023-03-26 21:31:43 -0600

    Removed odd naming

--- DND-MonstersASPNET/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace DND_MonstersASPNET.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
